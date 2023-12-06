5354045dd52026cc0a12781c500d527e0940f5e2
Plínio Netto <plinio.ice.pns@gmail.com>
2023-12-06 19:46:39 -0300

    fix: implements middleware to redirect in production

---
+++ app/Http/Middleware/ApiRedirect.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class ApiRedirect
+{
+    public function handle($request, \Closure $next)
+    {
+        // Check if the environment is production and the request URI contains "/api/"
+        if (app()->environment('production') && strpos($request->getRequestUri(), '/api/') !== false) {
+            // Redirect the request to "/api/api/"
+            $redirectedUri = str_replace('/api/', '/api/api/', $request->getRequestUri());
+
+            return redirect($redirectedUri, 301);
+        }
+
+        // Continue with the request for non-matching conditions
+        return $next($request);
+    }
+}
